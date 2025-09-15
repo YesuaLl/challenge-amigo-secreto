d78e62f8dd394a950f334bae5e28589bd0b77c9b
Yesua Llave <yesua3llave@outlook.com>
2025-09-15 13:51:42 -0600

    Create user array

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Se crea un array para almacenar los nombres
+let amigo = [];
