111bd6e43db62e7ee0c473794fc79022bbad7758
Yesua Llave <yesua3llave@outlook.com>
2025-09-15 15:20:13 -0600

    Implement addFriends function

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // Se crea un array para almacenar los nombres
-let amigo = [];
+let amigos = [];
+
+// Imprementacion  de funcion para agrgar amigos
+function agregarAmigo() {
+    //Se toma el valor de imput
+    let inputAmigo = document.getElementById('amigo').value.trim();
+
+    //Validacion
+    if (inputAmigo === '') {
+        alert('Por favor, inserte su nombre');
+        return;
+    }
+    //Se agrega al array
+    amigos.push(inputAmigo);
+
+    //limpia
+    document.getElementById('amigo').value = "";
+    
+    mostrarLista();
+}
+
+function mostrarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // limpiar lista
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
